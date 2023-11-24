a4affe2bb1b430501865ff09b6c62c8105de51ab
张忠源 <zhangzhongyuan@szanfu.cn>
2023-11-24 09:09:48 +0800

    feat:

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -6,17 +6,42 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-//头插法建立单链表 L为头指针 n为节点个数
+/**
+ * 头插法建立单链表
+ *  1.创建头节点(malloc)
+ *  2.头节点指针域置空
+ *  3.循环创建节点,并插入到头节点后面,即新节点的next指向头节点的next,头节点的next指向新节点
+ *
+ * @param L  头指针
+ * @param n  节点个数
+ */
 void CreateListHead(LinkList *L, int n) {
-  *L = (LinkList) malloc(sizeof(LNode));//头指针指向头节点
-  (*L)->next = NULL;                    //头节点的next指针为空
+  *L = (LinkList) malloc(sizeof(LNode));// 1.创建头节点(malloc)
+  (*L)->next = NULL;                    // 2.头节点指针域置空
+  LinkList p;
+  for (int i = 0; i < n; ++i) {// 3.循环创建节点,并插入到头节点后面,即新节点的next指向头节点的next,头节点的next指向新节点
+    p = (LinkList) malloc(sizeof(LNode));
+    p->data = i;
+    p->next = (*L)->next;
+    (*L)->next = p;
+  }
+}
 
-  //循环建立节点
-  LinkList p;                           //p为新建的节点 是个指针 指向新建的节点
-  for (int i = 0; i < n; i++) {
-    p = (LinkList) malloc(sizeof(LNode));//新建一个节点 是个指针 指向新建的节点
-    scanf("%d", &p->data);               //输入节点的值
-    p->next = (*L)->next;                //将新建的节点的next指针指向头节点的next指针指向的节点
-    (*L)->next = p;                      //将头节点的next指针指向新建的节点
+/**
+ * 尾插法建立单链表
+ *
+ * @param L  头指针
+ * @param n  节点个数
+ */
+void CreateListTail(LinkList *L, int n) {
+  *L = (LinkList) malloc(sizeof(LNode));  // 1.创建头节点(malloc)
+  (*L)->next = NULL;
+  LinkList p, tail = *L;
+  for (int i = 0; i < n; ++i) {                // 2.循环创建节点,并插入到尾节点后面,即新节点的next指向尾节点的next,尾节点的next指向新节点
+    p = (LinkList) malloc(sizeof(LNode));
+    p->data = i;
+    p->next = NULL;
+    tail->next = p;
+    tail = p;
   }
 }
